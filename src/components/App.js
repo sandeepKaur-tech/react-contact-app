2d3d2c5760d9e7d47d92df099a42f09358c8826d
sandeepKaur-tech <kaur94sandeep@gmail.com>
2024-08-23 14:51:56 +0400

    Add contact data to local storage and retrieve data from local storage

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,33 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import './App.css';
 import Header from './Header'
 import AddContact from './AddContact'
 import ContactList from './ContactList'
 
 function App() {
-    const [contacts, setContact] = useState([]) 
-    
+   const LOCAL_STORAGE_KEY= "contacts";
+    const [contacts, setContact] = useState(()=>{
+        return JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)) || []
+    }) 
+ 
 
 const addContactHandler = (contact)=>{
     console.log(contacts)
     setContact([...contacts,contact]);
 }
+// useEffect(()=>{
+//     const myData = localStorage.getItem(LOCAL_STORAGE_KEY);
+//     if(myData){
+//         setContact(JSON.parse(myData))
+//     }
+// },[]);
 
-    return(
+useEffect(()=>{
+    localStorage.setItem(LOCAL_STORAGE_KEY,JSON.stringify(contacts))
+},[contacts])
+   
+
+return(
     <div className="ui container">
         <Header />
         <AddContact addContactHandler={addContactHandler}/>
--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -2,7 +2,6 @@ import React from "react";
 import ContactCard from "./ContactCard";
 
 const ContactList = (contacts)=>{
-  console.log('www',contacts)
 
     const listItems = contacts.contacts.map(list=>
        <ContactCard contacts={list}/>
